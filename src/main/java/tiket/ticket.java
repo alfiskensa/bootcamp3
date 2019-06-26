d71ea289f6abee5bfac6a6bdad2089d214024b7a
derrysetya <derryprasetya77@gmail.com>
2019-06-26 11:11:45 +0700

    ticket

---
+++ src/main/java/tiket/ticket.java
@@ -0,0 +1,34 @@
+package tiket;
+
+public class ticket {
+	private String ticketName;
+	
+	public Boolean addTicket() {
+		return null;
+	}
+	
+	public Boolean editTicket() {
+		return null;
+	}
+	
+	public Boolean removeTicket() {
+		return null;
+	}
+
+	public String getTicketName(String str) {
+		return ticketName;
+	}
+
+	public void setTicketName(String ticketName) {
+		this.ticketName = ticketName;
+	}
+	
+	public void loadTicket() {
+		
+	}
+	
+	public void queryTicket() {
+		
+	}
+	
+}

a824953b5a7a8bc3b10103e2e329cbbc06590bce
rahmidewita <rahmidewita@gmail.com>
2019-06-26 11:56:19 +0700

    Venue

--- src/main/java/ticket/Venue.java
+++ src/main/java/ticket/Venue.java
@@ -2,9 +2,38 @@ package ticket;
 
 public class Venue {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	protected String venueName;
+	protected String venueLocation;
+	
+	public Venue() {
+	
+	}
+	public Boolean addVenue(){
+		return null;
+		}
+	public Boolean editVenue(){
+		return null;
+		}
+	public Boolean removeVenue(){
+		return null;
+		}
+	
+	
+	public String getVenueName() {
+		return venueName;
 	}
 
-}
+	public void setVenueName(String venueName) {
+		this.venueName = venueName;
+	}
+
+	public String getVenueLocation() {
+		return venueLocation;
+	}
+
+	public void setVenueLocation(String venueLocation) {
+		this.venueLocation = venueLocation;
+	}
+	
+	
+}		

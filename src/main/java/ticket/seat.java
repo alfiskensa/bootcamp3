ff05f40ed2ed67d18fe4a886c9965cb9b2186fe6
jhonfavosihombing <johnfavo.shb@gmail.com>
2019-06-26 11:48:19 +0700

    Seat

---
+++ src/main/java/ticket/seat.java
@@ -0,0 +1,27 @@
+package ticket;
+
+public class seat {
+  private String seatName;
+  
+  public Boolean addSeat() {
+	  return null;
+  }
+  
+  public Boolean editSeat() {
+	  return null;
+  }
+  
+  public Boolean removeSeat() {
+	  return null;
+  }
+
+public String getSeatName() {
+	return seatName;
+}
+
+public void setSeatName(String seatName) {
+	this.seatName = seatName;
+}
+  
+  
+}
